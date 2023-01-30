97ce4e5c3a2c01280eef1c6f9b0142604de34c74
Pavan Pabitwar <97040784+Pavan8010@users.noreply.github.com>
2023-01-30 12:17:52 +0530

    Time: 0 ms (100.00%), Space: 39 MB (70.39%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int tribonacci(int n) {
+        if(n==0){
+            return 0;
+        }
+        if(n==1 || n==2){
+            return 1;
+        }
+        int sum = 0;
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        for(int i=2;i<n;i++){
+            sum = a + b + c;
+            a = b;
+            b = c;
+            c = sum;
+        }
+        return sum;
+    }
+}
