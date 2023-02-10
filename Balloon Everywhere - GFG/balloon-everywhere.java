41d1c9c472f04a6dfe27ca2fa2d30f4f4c9dd20f
Pavan Pabitwar <97040784+Pavan8010@users.noreply.github.com>
2023-02-10 23:17:01 +0530

    Added solution - LeetHub

---
+++ Balloon Everywhere - GFG/balloon-everywhere.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// Position this line where user code will be pasted.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String s = br.readLine().trim();
+            Solution ob = new Solution();
+            int ans = ob.maxInstance(s);
+            out.println(ans);
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int maxInstance(String s) {
+        // Code here
+        Map<Character,Integer> map = new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        int count=0;
+        while(map.get('b')>=1 && map.get('a')>=1 && map.get('l')>=2 && map.get('o')>=2 && map.get('n')>=1){
+            count++;
+            map.put('b',map.get('b')-1);
+            map.put('a',map.get('a')-1);
+            map.put('l',map.get('l')-2);
+            map.put('o',map.get('o')-2);
+            map.put('n',map.get('n')-1);
+        }
+        return count;
+    }
+}
