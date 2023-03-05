dc8924caa0b0b85c33c2004cb7e915f43302118e
Pavan Pabitwar <97040784+Pavan8010@users.noreply.github.com>
2023-03-05 18:37:44 +0530

    Time: 89 ms (46.27%), Space: 60.1 MB (42.43%) - LeetHub

---
+++ 1345-jump-game-iv/1345-jump-game-iv.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int minJumps(int[] arr) {
+        Map<Integer,ArrayList<Integer>>hm=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            if(hm.containsKey(arr[i])){
+                ArrayList<Integer>al=hm.get(arr[i]);
+                al.add(i);
+                hm.put(arr[i],al);
+            }
+            else{
+                ArrayList<Integer>al=new ArrayList<>();
+                al.add(i);
+                hm.put(arr[i],al);
+            }
+        }
+        // System.out.println(hm);
+        boolean vis[] = new boolean[arr.length];
+        Queue<Integer> q = new LinkedList<>();
+        q.add(0);
+        int cnt=0;
+        System.out.println(q);
+        while(!q.isEmpty()){
+            for(int k = q.size();k>0;k--){
+                int n = q.remove();
+                vis[n] = true;
+                if(n==arr.length-1) return cnt;
+                ArrayList<Integer> al = hm.get(arr[n]);
+                al.add(n-1);
+                al.add(n+1);
+                for(int i : al){
+                    if(i>=0 && i<arr.length && !vis[i]){
+                        q.add(i);
+                    }
+                }
+                al.clear();
+            }
+            // System.out.println(hm);
+            // System.out.println(q);
+            cnt++;
+        }
+        return 0;
+    }
+}
