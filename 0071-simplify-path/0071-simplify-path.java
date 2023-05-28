080005167da144fabc37014601faaacf2e621522
Pavan Pabitwar <97040784+Pavan8010@users.noreply.github.com>
2023-05-28 15:14:54 +0530

    Time: 5 ms (55.38%), Space: 43.6 MB (5.93%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,36 +1,23 @@
 class Solution {
    public String simplifyPath(String path) {
-    Stack<String> stack = new Stack<>();
-    Set<String> skip = new HashSet<>(Arrays.asList("..", ".", ""));
-    for (String dir : path.split("/")) {
-        if (dir.equals("..") && !stack.isEmpty())
-            stack.pop();
-        else if (!skip.contains(dir))
-            stack.push(dir);
-    }
-    StringBuilder res = new StringBuilder();
-    while(!stack.isEmpty()){
-        res.insert(0,stack.pop()).insert(0,"/");
-    }
-    return res.isEmpty() ? "/" : res.toString();
+        Stack<String> stack = new Stack<>();
+        Set<String> skip = new HashSet<>(Arrays.asList("..", ".", ""));
+        String[] arr = path.split("/");
+        for (String dir : arr) {
+            if (dir.equals("..") && !stack.isEmpty()){
+                stack.pop();
+            }
+            else if (!skip.contains(dir)){
+                stack.push(dir);
+            }
+        }
+        StringBuilder res = new StringBuilder();
+        while(!stack.isEmpty()){
+            res.insert(0,stack.pop()).insert(0,"/");
+        }
+        if(res.isEmpty()){
+            return "/";
+        }
+        return res.toString();
     }
 }
-        // String[] str = path.split("/");
-        // // String ans = "/";
-        // StringBuilder ans = new StringBuilder();
-        // ans.append("/");
-        // String dot = ".",ddot = "..";
-        // int n = str.length;
-        // for(int i=0;i<n;i++){
-        //     String s = str[i];
-        //     if(s.equals(dot) || s.equals(ddot)){
-        //         continue;
-        //     }
-        //     ans.append(s);
-        //     ans.append("/");
-        //     // if(i != n-1){
-        //     //     // ans += "/";
-        //     // }
-        // }
-        // ans.deleteCharAt(ans.length()-1);
-        // return ans.toString();
