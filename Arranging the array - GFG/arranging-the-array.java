d863518f4820eacda4a929d5947e36529e681f66
Pavan Pabitwar <97040784+Pavan8010@users.noreply.github.com>
2023-06-10 17:07:39 +0530

    Added solution - LeetHub

---
+++ Arranging the array - GFG/arranging-the-array.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();
+            obj.Rearrange(a, n);
+            
+            StringBuilder output = new StringBuilder();
+            for(int i=0;i<n;i++)
+                output.append(a[i]+" ");
+            System.out.println(output);
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    
+    public void Rearrange(int arr[], int n){
+        // Your code goes here
+        int i=0;
+        List<Integer> list = new ArrayList<>();
+        for(int j=0;j<n;j++){
+            if(arr[j]<0){
+                arr[i] = arr[j];
+                i++;
+            }else{
+                list.add(arr[j]);
+            }
+        }
+        for(int num:list){
+            arr[i] = num;
+            i++;
+        }
+    }
+}
