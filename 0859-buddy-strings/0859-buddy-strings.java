52e6ef63454903af32a066fd63bbdc895f41e34d
Pavan Pabitwar <97040784+Pavan8010@users.noreply.github.com>
2023-07-03 15:01:25 +0530

    Time: 12 ms (6.09%), Space: 43.7 MB (5.67%) - LeetHub

---
+++ 0859-buddy-strings/0859-buddy-strings.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean buddyStrings(String s, String goal) {
+        int cnt=0;
+        char check1='a',check2='a';
+        if(s.length()!=goal.length())return false;
+ 
+        Map<Character,Integer> map = new HashMap<>();
+        for(char ch:s.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) != goal.charAt(i)){
+                if(cnt==0){
+                    check1 = s.charAt(i);
+                    check2 = goal.charAt(i);
+                    cnt++;
+                }else if(s.charAt(i)==check2 && goal.charAt(i)==check1){
+                    cnt++;
+                }
+                else return false;
+            }
+        }
+        if(cnt==0){
+            for(char ch: map.keySet()){
+                if(map.get(ch)>1)return true;
+            }
+        }
+ 
+        return cnt==2;
+    }
+}
