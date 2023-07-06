e87bcfe6baa2e7e15be7e5fa22175fe179e92bfd
Pavan Pabitwar <97040784+Pavan8010@users.noreply.github.com>
2023-07-06 12:10:57 +0530

    Time: 2 ms (18.99%), Space: 56 MB (30.24%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,44 +1,20 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        int[] cummu = new int[nums.length +1];
-        int sum = 0;
-        for(int i=0;i<nums.length;i++){
-            sum += nums[i];
-            cummu[i+1] = sum;
-        }
-        if(sum<target){
-            return 0;
-        }
-        int ans = Integer.MAX_VALUE;
-        for(int i=0;i<nums.length;i++){
-            int num = cummu[i+1] - target;
-            int idx = upper_bound(cummu,0,i,num);
-            if(idx != -1){
-                ans = Math.min(ans,i-idx+1);
+        int n = nums.length;
+        int left=0,currSum=0,ans=n+1;
+        for(int right=0;right<nums.length;right++){
+            currSum += nums[right];
+
+            while(left<right && (currSum-nums[left])>=target){
+                currSum -= nums[left];
+                left++;
+            }
+
+            if(currSum>=target){
+                ans = Math.min(ans,right-left+1);
             }
         }
+        if(ans==n+1)return 0;
         return ans;
     }
-    public int upper_bound(int arr[], int l, int r, int x){
-    while(l <= r){
-        int mid = l + (r-l)/2;
-        if(arr[mid] == x){
-            return mid;  
-        }
-        else if(arr[mid] < x){
-            if(arr[mid+1] > x){
-                return mid;
-            }
-            l = mid+1;
-        } 
-        else {        
-            r = mid-1;
-        }
-    }
-    return -1;
-    }
 }
-        // for(int i:cummu){
-        //     System.out.print(i+" ");
-        // }
-        // System.out.println(upper_bound(cummu,target));
