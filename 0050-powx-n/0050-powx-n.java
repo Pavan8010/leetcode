bac6c8ac8bbedb03ae062f1173b4aa651c6f81e2
Pavan Pabitwar <97040784+Pavan8010@users.noreply.github.com>
2023-07-24 12:57:36 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (40.10%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,11 @@
+class Solution {
+    public double myPow(double x, long n) {
+        if(n==0 || x==1) return 1;
+
+        if(x==0) return 0;
+        
+        if(n<0) return myPow(1/x,-n);
+
+        return (n%2==0 ? myPow(x*x,n/2) : x*myPow(x*x,n/2));
+    }
+}
