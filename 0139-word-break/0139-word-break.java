d477a7b3875b142f9983bd8eb5312ce7c2a7f259
Pavan Pabitwar <97040784+Pavan8010@users.noreply.github.com>
2023-08-04 23:25:09 +0530

    Time: 6 ms (64.15%), Space: 43.5 MB (54.20%) - LeetHub

---
+++ 0139-word-break/0139-word-break.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    Map<String, Boolean> dp;
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> m = new HashSet<>(wordDict);
+        dp = new HashMap<>();
+        return solve(s, m);
+    }
+    public boolean solve(String s, Set<String> m) {
+        if (s.length() == 0) return true;
+        if (dp.containsKey(s)) return dp.get(s);
+        
+        for (int i = 0; i < s.length(); i++) {
+            String r = s.substring(0, i + 1);
+            if (m.contains(r)) {
+                if (solve(s.substring(i + 1), m)){
+                    dp.put(s,true);
+                    return true;
+                }
+            }
+        }
+        dp.put(s, false);
+        return false;
+    }
+}
