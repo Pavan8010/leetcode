a8d9ab7febcc066f0c2aa2e389801e88649a183d
Pavan Pabitwar <97040784+Pavan8010@users.noreply.github.com>
2023-08-20 20:44:15 +0530

    Time: 4 ms (66.87%), Space: 43.5 MB (84.50%) - LeetHub

---
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] deckRevealedIncreasing(int[] deck) {
+        int n= deck.length;
+        Arrays.sort(deck);
+        Queue<Integer> q= new LinkedList<>();
+        for (int i=0; i<n; i++) q.add(i);
+        int[] res= new int[n];
+        for (int i=0; i<n; i++){
+            res[q.poll()]=deck[i];
+            q.add(q.poll());
+        }
+        return res;
+    }
+}
+
+// 17
+// 17 13  
+// 13 11 17  
+// 17 7 13 11   
