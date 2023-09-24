d0833ac3ff5414915be05b7ed73fbad5e26db6cd
Pavan Pabitwar <97040784+Pavan8010@users.noreply.github.com>
2023-09-24 23:14:43 +0530

    Time: 4 ms (57.57%), Space: 43.6 MB (26.65%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.java
+++ 0799-champagne-tower/0799-champagne-tower.java
@@ -3,11 +3,11 @@ class Solution {
         double[][] ans = new double[101][101];
         ans[0][0]=poured;
         for(int i=0;i<100;i++){
+            if(i>row)return ans[row][glass];
             for(int j=0;j<=i;j++){
                 if(ans[i][j]>1){
                     ans[i+1][j] += (ans[i][j]-1)/2.0;
                     ans[i+1][j+1] += (ans[i][j]-1)/2.0;
-
                     ans[i][j]=1;
                 }
             }
